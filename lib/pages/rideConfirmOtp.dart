5fa632cddf47e9bd98a6bd43cdac024f1109cd5e
bhavesh982 <bhaveshkunagvanshi@gmail.com>
2023-11-25 09:54:06 +0530

    FirebaseAnimatedList

--- lib/pages/rideConfirmOtp.dart
+++ lib/pages/rideConfirmOtp.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:login/commons/common_methods.dart';
 import 'package:login/global/global_var.dart';
@@ -15,6 +16,18 @@ class RideConfirmOTP extends StatefulWidget {
 class _RideConfirmOTPState extends State<RideConfirmOTP> {
  CommonMethods commonMethods=CommonMethods();
  Random rand=Random();
+ String eventSnap="";
+ @override
+ void initState() {
+   DatabaseReference dref=FirebaseDatabase.instance.ref().child("owners").child(ownerUID).child("riderequest").child(spaceShipName).child(useruid);
+   dref.onValue.listen((event) {
+     setState(() {
+       eventSnap=(event.snapshot.value as Map)["status"];
+     });
+   });
+   commonMethods.displaySnackBar(eventSnap, context);
+   super.initState();
+ }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,27 +36,42 @@ class _RideConfirmOTPState extends State<RideConfirmOTP> {
         backgroundColor: const Color(0xff103232),
       ),
       body: SingleChildScrollView(
-        child: Column(
+        child: eventSnap!="confirmed"?Column(
           mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                const SizedBox(height: 50,),
-                const Padding(
-                  padding: EdgeInsets.only(top: 40,left: 40),
-                  child: ListTile(
-                    title: Text("Secret code!",style: TextStyle(
-                      fontSize: 22
-                    ),),
-                    subtitle: Text("for your journey."),
-                  ),
-                ),
-              Container(height: 100,
-                width: 100,
-                child: Text(generatedOtp.toString(),style: const TextStyle(
-                  fontSize: 35
+          children: [
+            const SizedBox(height: 50,),
+            const Padding(
+              padding: EdgeInsets.only(top: 40,left: 40),
+              child: ListTile(
+                title: Text("Secret code!",style: TextStyle(
+                    fontSize: 22
                 ),),
+                subtitle: Text("for your journey."),
               ),
-              const SizedBox(height: 150,)
-            ],
+            ),
+            Container(height: 100,
+              width: 100,
+              child: Text(generatedOtp.toString(),style: const TextStyle(
+                  fontSize: 35
+              ),),
+            ),
+            const SizedBox(height: 150,)
+          ],
+        ):const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50,),
+            Padding(
+              padding: EdgeInsets.only(top: 40,left: 40),
+              child: ListTile(
+                title: Text("Happy Journey",style: TextStyle(
+                    fontSize: 22
+                ),),
+                subtitle: Text("Make Sure to Not just die or something "),
+              ),
+            ),
+            SizedBox(height: 150,)
+          ],
         ),
       ),
     );
